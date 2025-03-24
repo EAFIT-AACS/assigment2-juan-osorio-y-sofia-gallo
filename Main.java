8940be977c3b9a664218de7f1704b48cf0badb5f
efcortesr <159450837+efcortesr@users.noreply.github.com>
2025-03-23 23:22:08 -0500

    Main.java

--- Main.java
+++ Main.java
@@ -11,12 +11,12 @@ public class Main {
         System.out.println("----------------------------------");
         System.out.println("Cadenas válidas generadas:");
         for (int i = 0; i < 5; i++) {
-            validStrings[i] = StringGenerator.generateValidString();
+            validStrings[i] = ALGORITHM_1_LFCO_2025_JJOA_SG_EFCR.generateValidString();
             System.out.println((i+1) + ". '" + validStrings[i] + "'");
         }
         System.out.println("\nCadenas inválidas generadas:");
         for (int i = 0; i < 5; i++) {
-            invalidStrings[i] = StringGenerator.generateInvalidString();
+            invalidStrings[i] = ALGORITHM_1_LFCO_2025_JJOA_SG_EFCR.generateInvalidString();
             System.out.println((i+1) + ". '" + invalidStrings[i] + "'");
         }
 
@@ -25,7 +25,7 @@ public class Main {
         System.out.println("Verificación de cadenas válidas:");
         for (int i = 0; i < validStrings.length; i++) {
             List<String> steps = new ArrayList<>();
-            boolean accepted = PDA.accepts(validStrings[i], steps);
+            boolean accepted = ALGORITHM_2_LFCO_2025_JJOA_SG_EFCR.accepts(validStrings[i], steps);
             System.out.println("\nCadena: '" + validStrings[i] + "' - " + 
                               (accepted ? "ACEPTADA ✓" : "RECHAZADA ✗"));
             System.out.println("Pasos del PDA:");
@@ -36,7 +36,7 @@ public class Main {
         System.out.println("\nVerificación de cadenas inválidas:");
         for (int i = 0; i < invalidStrings.length; i++) {
             List<String> steps = new ArrayList<>();
-            boolean accepted = PDA.accepts(invalidStrings[i], steps);
+            boolean accepted = ALGORITHM_2_LFCO_2025_JJOA_SG_EFCR.accepts(invalidStrings[i], steps);
             System.out.println("\nCadena: '" + invalidStrings[i] + "' - " + 
                               (accepted ? "ACEPTADA ✓" : "RECHAZADA ✗"));
             System.out.println("Pasos del PDA:");
@@ -49,19 +49,19 @@ public class Main {
         System.out.println("--------------------------------------------------");
         for (int i = 0; i < validStrings.length; i++) {
             List<String> steps = new ArrayList<>();
-            boolean accepted = PDA.accepts(validStrings[i], steps);
+            boolean accepted = ALGORITHM_2_LFCO_2025_JJOA_SG_EFCR.accepts(validStrings[i], steps);
 
             if (accepted) {
                 System.out.println("\nCadena válida: '" + validStrings[i] + "'");
                 System.out.println("Derivación por la izquierda:");
 
-                List<String> derivation = DerivationTree.generateLeftmostDerivation(validStrings[i]);
+                List<String> derivation = ALGORITHM_3_LFCO_2025_JJOA_SG_EFCR.generateLeftmostDerivation(validStrings[i]);
                 for (int j = 0; j < derivation.size(); j++) {
                     System.out.println((j+1) + ". " + derivation.get(j));
                 }
 
                 System.out.println("\nÁrbol de derivación:");
-                DerivationTree.drawDerivationTree(validStrings[i]);
+                ALGORITHM_3_LFCO_2025_JJOA_SG_EFCR.drawDerivationTree(validStrings[i]);
             }
         }
     }
